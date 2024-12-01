ac7dd177b03b4e6ebb2a9ac66ead9175546dac5b
JairoSantanaA <jairoarielsantanaaros@gmail.com>
2024-12-01 20:46:25 -0300

    Medico

--- models/people/medico.py
+++ models/people/medico.py
@@ -5,3 +5,20 @@ class Medico:
         self.telefono = telefono
         self.especialidad = especialidad
         self.correo = correo
+        self.horarios = []
+        self.reservas = []
+
+    def agregar_horario(self, horario):
+        self.horarios.append(horario)
+
+    def hacer_reserva(self, fecha, hora, paciente):
+        for horario in self.horarios:
+            if horario.dia == fecha and horario.hora_inicio <= hora <= horario.hora_fin:
+                reserva = Reserva(self, fecha, hora, paciente)
+                self.reservas.append(reserva)
+                return f"Reserva hecha para {paciente} con {self.nombre} el {fecha} a las {hora}."
+        return "Horario no disponible."
+
+    def mostrar_reservas(self):
+        for reserva in self.reservas:
+            print(f"Reserva: {reserva.fecha} a las {reserva.hora} para {reserva.paciente}.")
