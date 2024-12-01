b3de1980dfe39ddc1511475e53f901419d246bd6
Benja1-col <benjasolis0291@gmail.com>
2024-12-01 20:30:11 -0300

    Update medico.py

--- models/people/medico.py
+++ models/people/medico.py
@@ -1,7 +1,1 @@
-CREATE TABLE medicos (
-    id INT PRIMARY KEY,  -- ID del médico (clave foránea que referencia a usuarios.id)
-    especialidad VARCHAR(100),  -- Especialidad del médico
-    consultorio VARCHAR(100),  -- Consultorio del médico
-    disponibilidad TEXT,  -- Horarios de disponibilidad
-    FOREIGN KEY (id) REFERENCES usuarios(id) ON DELETE CASCADE -- Relación con usuarios
-);
+
