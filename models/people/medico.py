ff724b4bb17b70a02b10c75672d21dcb40fc86ac
JairoSantanaA <jairoarielsantanaaros@gmail.com>
2024-12-01 20:37:11 -0300

    Medico

--- models/people/medico.py
+++ models/people/medico.py
@@ -1,1 +1,7 @@
-
+class Doctor:
+    def __init__(self, id, nombre, telefono, especialidad, correo):
+        self.id = id
+        self.nombre = nombre
+        self.telefono = telefono
+        self.especialidad = especialidad
+        self.correo = correo
