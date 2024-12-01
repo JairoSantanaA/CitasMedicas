00682aadc6184cd2ca81525586af49d69044a1b2
Benja1-col <benjasolis0291@gmail.com>
2024-12-01 20:26:55 -0300

    Update paciente.py

--- models/people/paciente.py
+++ models/people/paciente.py
@@ -1,7 +1,13 @@
-CREATE TABLE pacientes (
-    id INT PRIMARY KEY,  -- ID del paciente (clave foránea que referencia a usuarios.id)
-    historial_medico TEXT,  -- Historial médico del paciente
-    direccion VARCHAR(255),  -- Dirección del paciente
-    telefono VARCHAR(15),  -- Teléfono del paciente
-    FOREIGN KEY (id) REFERENCES usuarios(id) ON DELETE CASCADE -- Relación con usuarios
-);
+class Paciente:
+    def __init__(self, nombre, apellido, telefono, email, historial):
+        super().__init__(nombre, apellido, telefono, email)
+        self.historial = historial
+
+    def agendar_cita(self, cita):
+        print(f'{self.nombre} {self.apellido} ha agendado una cita con el Dr. {cita.medico.nombre}')
+
+    def consultar_historial(self):
+        return self.historial
+
+    def __str__(self):
+        return super().__str__()
